717898217c8e5762c4d92d935924952dad01fe43
Rachid Amghar <amghar.rachid@gmail.com>
2023-01-04 14:44:25 -0800

    Configuring the Course Project.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals'; removed
 import './index.css';
+
+// I added these from lecture.
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import 'font-awesome/css/font-awesome.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
