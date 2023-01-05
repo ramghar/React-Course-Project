bb53ce18ca5f8cc7828c64077fae0e4fa569704b
Rachid Amghar <amghar.rachid@gmail.com>
2023-01-05 10:18:36 -0800

    The App Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+
 import React from 'react';
-//import logo from './logo.svg'; removed
-//import { Counter } from './features/counter/Counter'; removed
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello nucamp!
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={NucampLogo} alt='nucamp logo' />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      I'm ready for workshop!
     </div>
   );
 }
